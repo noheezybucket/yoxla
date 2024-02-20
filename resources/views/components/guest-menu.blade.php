e1184eefe5c62ebe4fda992390d0f525bf507529
Mouhamad Gueye <seydinag023@gmail.com>
2024-02-20 22:57:18 +0000

    homepage is ok

--- resources/views/components/guest-menu.blade.php
+++ resources/views/components/guest-menu.blade.php
@@ -1,1 +1,24 @@
-guest-menu-works
+<header class="text-gray-600 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span class="ml-3 text-5xl font-bold">Yoxlā</span>
+        </a>
+
+        <div class="flex gap-5">
+
+            <a href="{{ route('auth.login') }}"
+                class="inline-flex items-center bg-first text-white border-0 py-3 px-3 focus:outline-none hover:bg-third rounded-2xl text-base mt-4 md:mt-0">Se
+                connecter
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+            </a>
+
+            {{-- <a href=""
+                class="inline-flex items-center bg-accent1 text-white border-0 py-3 px-3 focus:outline-none hover:bg-third rounded-2xl text-base mt-4 md:mt-0">Démonstration
+
+            </a> --}}
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::view('login', 'auth.login')->name('login');
+});
