bd49673e0f93bd24f65f3de601863c9063413eba
Mouhamad Gueye <seydinag023@gmail.com>
2024-02-22 21:43:00 +0000

    generated vehicles drivers and rentals table

---
+++ database/migrations/2024_02_22_203535_create_drivers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->integer('years_of_xp');
+            $table->integer('avg_rating');
+            $table->bigInteger('license_number');
+            $table->bigInteger('phonenumber');
+            $table->enum('license_category', ['A', 'B', 'AB']);
+            $table->enum('status', ['disponible', 'indisponible']);
+            $table->date('license_emission_date');
+            $table->date('license_expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drivers');
+    }
+};
